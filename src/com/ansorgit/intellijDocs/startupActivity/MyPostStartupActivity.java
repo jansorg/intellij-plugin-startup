ff40f2a14800f6fb5078afa366d7c8c5c68f3708
jansorg <github@joachim-ansorg.de>
2017-04-25 11:44:05 +0200

    Added some sample logging to the post startup activity

--- src/com/ansorgit/intellijDocs/startupActivity/MyPostStartupActivity.java
+++ src/com/ansorgit/intellijDocs/startupActivity/MyPostStartupActivity.java
@@ -1,5 +1,6 @@
 package com.ansorgit.intellijDocs.startupActivity;
 
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.startup.StartupActivity;
 import org.jetbrains.annotations.NotNull;
@@ -8,7 +9,9 @@ import org.jetbrains.annotations.NotNull;
  * A {@link StartupActivity} to demonstrate how a plugin can execute some code after a project has been opened.
  */
 public class MyPostStartupActivity implements StartupActivity {
-    public void runActivity(@NotNull Project project) {
+    private static final Logger LOG = Logger.getInstance(MyPostStartupActivity.class);
 
+    public void runActivity(@NotNull Project project) {
+        LOG.info("Project startup activity");
     }
 }
